ae8a3e5d585820f14f53444f84211b6a4aa13acf
Pablo Rios <pablorios1603@gmail.com>
2021-06-16 20:33:43 -0500

    Se agrego el consumo de la API

---
+++ src/components/Currencies.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="currencies">
+    <div class="row">
+      <div class="" v-for="c in cryptos" :key="c.id">
+        <div class="col-12">
+          <b-card
+            :title="c.id"
+            :img-src="c.logo_url"
+            img-alt="Image"
+            img-top
+            tag="article"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Currencies",
+  data: () => ({
+    cryptos: [],
+    errors: [],
+  }),
+  created() {
+    axios
+      .get(
+        "https://api.nomics.com/v1/currencies/ticker?key=1a2b63f7fe249f264cf860de3e9ff912838c5f1b&interval=1d,30d&per-page=20&page=1"
+      )
+      .then((response) => {
+        this.cryptos = response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+
+  props: {},
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="currencies">    
+    <Currencies/>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import Currencies from '../components/Currencies.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Currencies
   },
 };
 </script>
